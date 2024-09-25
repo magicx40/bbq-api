502d601a6bed438174249b9e3dad631f2d2a44fc
hanzongyuan <hanzongyuan@foxmail.com>
2024-09-25 23:34:25 +0800

    feat: 添加加密库

---
+++ src/util/bcrypt.ts
@@ -0,0 +1,9 @@
+import * as bcrypt from 'bcrypt';
+export async function password_hash(password: string) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
+
+export async function comparePassword(password: string, hash: string) {
+  return await bcrypt.compare(password, hash);
+}
